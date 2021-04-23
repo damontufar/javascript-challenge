d553db7229b05ef560da66feea41ecf0b1b0dbdc
Daniela Marroquín <daniela.montufar18@gmail.com>
2021-04-23 16:08:36 -0500

    Creating striped table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,10 +1,18 @@
 // from data.js
 let tableData = data;
 
+//Use D3 to select the table
+let table = d3.select("table")
+
+// Use d3 to create a bootstrap striped table
+// http://getbootstrap.com/docs/3.3/css/#tables-striped
+table.attr("class", "table table-striped");
+
+
 //Reference to the table body
 let tbody = d3.select("tbody");
 
-//
+///Appending Table
 
 data.forEach((row) => {
     let newtr= tbody.append("tr");
